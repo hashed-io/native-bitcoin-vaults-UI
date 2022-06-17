84af532e302801190b3208cd13304ef1a47dfa36
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-17 13:52:16 -0500

    Modifed the Stories to add Interaction & Actions

--- src/stories/common/account-input.stories.js
+++ src/stories/common/account-input.stories.js
@@ -1,6 +1,6 @@
 import AccountInput from '../../components/common/account-input.vue'
 import { validation } from '~/mixins/validation'
-
+import { userEvent, within } from '@storybook/testing-library'
 export default {
   title: 'Common/AccountInput',
   component: AccountInput
@@ -21,7 +21,7 @@ const Template = (args) => ({
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup () {
     // Story args can be spread into the returned object
-    return { ...args }
+    return { args }
   },
   // Then, the spread values can be accessed directly in the template
   template: '<accountInput v-model="data" :rules="[rules.required, rules.isValidPolkadotAddress]"/>'
@@ -31,4 +31,23 @@ export const Base = Template.bind({})
 Base.args = {
   label: 'Account Input Test',
   outlined: true
+}
+export const SuccessValidation = Template.bind({})
+SuccessValidation.args = {
+  label: 'Account Input Test',
+  outlined: true
+}
+const addressAccount = '5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym'
+SuccessValidation.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.type(canvas.getByTestId('account_input', addressAccount))
+}
+export const FailedValidation = Template.bind({})
+FailedValidation.args = {
+  label: 'Account Input Test',
+  outlined: true
+}
+FailedValidation.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.clear(canvas.getByTestId('account_input'))
 }
--- src/stories/proposals/create-proposal-form.stories.js
+++ src/stories/proposals/create-proposal-form.stories.js
@@ -1,8 +1,12 @@
 import CreateProposalForm from '../../components/proposals/create-proposal-form.vue'
-
+import { userEvent, within } from '@storybook/testing-library'
+import { action } from '@storybook/addon-actions'
 export default {
   title: 'Proposals/CreateProposalForm',
-  component: CreateProposalForm
+  component: CreateProposalForm,
+  argTypes: {
+    submitForm: { action: action('submitForm') }
+  }
 }
 
 const Template = (args) => ({
@@ -13,10 +17,33 @@ const Template = (args) => ({
     // Story args can be spread into the returned object
     return { ...args }
   },
+  methods: {
+    submitForm: action('submitForm')
+  },
   // Then, the spread values can be accessed directly in the template
-  template: '<CreateProposalForm/>'
+  template: '<CreateProposalForm @submittedForm="submitForm"/>'
 })
 
 export const Base = Template.bind({})
 Base.args = {
+}
+// Fail creation
+export const FailCreation = Template.bind({})
+FailCreation.args = {}
+FailCreation.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.clear(canvas.getByTestId('description'))
+  await userEvent.type(canvas.getByTestId('recipient'), 'Invalid BTC Address [required]')
+  await userEvent.type(canvas.getByTestId('amount'), 'Invalid amount [required]')
+  await userEvent.click(canvas.getByTestId('submitButton'), 'Create Proposal')
+}
+// Success creation
+export const SuccessCreation = Template.bind({})
+SuccessCreation.args = {}
+SuccessCreation.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.type(canvas.getByTestId('description'), 'Test proposal')
+  await userEvent.type(canvas.getByTestId('recipient'), 'bc1q89fquejvzxqaq7cf3fuhtghw0p59jld5tja3vj')
+  await userEvent.type(canvas.getByTestId('amount'), '1000')
+  await userEvent.click(canvas.getByTestId('submitButton'))
 }
